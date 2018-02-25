6b40ea42ab326206dae5c15deb422facb4957b8f
T.MAHARANI <maharani1118@gmail.com>
2018-02-25 15:51:01 +0530

    Create Hello.java

---
+++ Basic Program/Hello.java
@@ -0,0 +1,21 @@
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+/*
+ * uncomment this if you want to read input.
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility classes
+import java.util.*;
+*/
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+       
+        System.out.print("Hello Kirti");
+
+    }
+}
